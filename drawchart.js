0d7325147b4bc7368f11949a22bd681aa4606acf
Andreas <ahindman@uw.edu>
2020-07-11 23:54:31 -0700

    added intermediate function

--- drawchart.js
+++ drawchart.js
@@ -1,5 +1,4 @@
 var googleSheetURL = document.getElementById("sheetsURL").value;
-// var googleSheetURL = 'https://docs.google.com/spreadsheets/d/1i2VsyFFcLQb-wC1nzh8sIKp70_yrns7rvj-H9myBDtg/edit?usp=sharing';
 
 function init() {
     Tabletop.init( { key: googleSheetURL, 
---
+++ generatechart.js
@@ -0,0 +1,5 @@
+function setupChart() {
+    var googleSheetURL = document.getElementById("sheetsURL").value;
+    // var googleSheetURL = 'https://docs.google.com/spreadsheets/d/1i2VsyFFcLQb-wC1nzh8sIKp70_yrns7rvj-H9myBDtg/edit?usp=sharing';
+    init();
+}
