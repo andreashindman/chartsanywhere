def2d2e4a3fca40ac4c139b9489aacb18e5edd30
Andreas <ahindman@uw.edu>
2020-07-14 15:08:00 -0700

    display JavaScript and some styling improvements

--- generatechart.js
+++ generatechart.js
@@ -1,14 +1,18 @@
 function setupChart() {
     // var googleSheetURL = document.getElementById("sheetsURL").value;
     // var googleSheetURL = 'https://docs.google.com/spreadsheets/d/1i2VsyFFcLQb-wC1nzh8sIKp70_yrns7rvj-H9myBDtg/edit?usp=sharing';
+
+    removeChart();
     init();
-    
     readTextFile("./drawChart.js");
+}
+function removeChart() {
+    console.log("This function should remove chart");
 }
 
 function addStringToPage(s) {
     var jsTextNode = document.createTextNode(s);
-    document.getElementById("js-text").appendChild(jsTextNode);
+    document.getElementById("js-text").innerHTML = s;
 }
 
 function readTextFile(file) {
